819d42be9223510a4eb9ae2977dc9c3a4a3d1f04
Cameron Edgerton <cam.edgerton@gmail.com>
2024-07-27 19:10:36 +1000

    files cascade if folder deleted

--- FileFolderExplorer/Repositories/FileFolderExplorerContext.cs
+++ FileFolderExplorer/Repositories/FileFolderExplorerContext.cs
@@ -8,16 +8,19 @@ public class FileFolderExplorerContext(DbContextOptions<FileFolderExplorerContex
 {
     public DbSet<Folder> Folders { get; set; }
     public DbSet<File> Files { get; set; }
-    
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Folder>(entity =>
         {
             entity.HasKey(f => f.FolderId);
             entity.Property(f => f.Name).IsRequired();
+
             entity.HasMany(f => f.Files)
                 .WithOne(f => f.Folder)
-                .HasForeignKey(f => f.FolderId);
+                .HasForeignKey(f => f.FolderId)
+                .OnDelete(DeleteBehavior.Cascade);
+
             entity.HasMany(f => f.Subfolders)
                 .WithOne(f => f.ParentFolder)
                 .HasForeignKey(f => f.ParentFolderId)
@@ -29,9 +32,11 @@ public class FileFolderExplorerContext(DbContextOptions<FileFolderExplorerContex
             entity.HasKey(f => f.FileId);
             entity.Property(f => f.Name).IsRequired();
             entity.Property(f => f.Content).IsRequired();
+
             entity.HasOne(f => f.Folder)
                 .WithMany(f => f.Files)
-                .HasForeignKey(f => f.FolderId);
+                .HasForeignKey(f => f.FolderId)
+                .OnDelete(DeleteBehavior.Cascade);
         });
     }
 }
